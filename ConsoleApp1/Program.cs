1173823f64ff7aa63de3e0dd000e48e2b021e3b3
LinhLPV <lephamvanlinh@gmail.com>
2018-12-05 19:53:12 +0700

    Update Child for Test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,19 +3,40 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 using Raven.Client.Documents;
 using Raven.Client.Documents.BulkInsert;
 
 namespace ConsoleApplication6
 {
+    public class Child
+    {
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public Father Father { get; set; }
+        public Mother Mother { get; set; }
+    }
+
+    public class Father
+    {
+        public string Name { get; set; }
+    }
+
+    public class Mother
+    {
+        public string Name { get; set; }
+    }
     internal class Program
     {
+
         public class User
         {
             public int Score;
             public string Name;
             public DateTime CreatedAt;
         }
+
+        
 
         private static char[] _buffer = new char[6];
         private static string RandomName(Random rand)
@@ -32,23 +53,60 @@ namespace ConsoleApplication6
             using (var store = new DocumentStore
             {
                 Urls = new[] { "http://localhost:8080" },
-                Database = "bench"
+                Database = "Test"
             }.Initialize())
             {
                 var sp = Stopwatch.StartNew();
+                //using (var bulk = store.BulkInsert())
+                //{
+                //    var rand = new Random();
+                //    for (int i = 0; i < 10000 * 1000; i++)
+                //    {
+                //        bulk.Store(new User
+                //        {
+                //            CreatedAt = DateTime.Today.AddDays(rand.Next(356)),
+                //            Score = rand.Next(0, 5000),
+                //            Name = RandomName(rand)
+                //        });
+                //    }
+                //}
+                //Parallel.For(0, 5, i =>
+                //{
+                //    using (var bulk = store.BulkInsert())
+                //    {
+                //        var rand = new Random();
+                //        for (int j = 0; j < 20000 * 1000; j++)
+                //        {
+                //            bulk.Store(new User
+                //            {
+                //                CreatedAt = DateTime.Today.AddDays(rand.Next(356)),
+                //                Score = rand.Next(0, 5000),
+                //                Name = RandomName(rand)
+                //            });
+                //        }
+                //    }
+                //});
                 using (var bulk = store.BulkInsert())
                 {
                     var rand = new Random();
-                    for (int i = 0; i < 10000 * 1000; i++)
+                    for (int i = 0; i < 100 * 1000; i++)
                     {
-                        bulk.Store(new User
+                        bulk.Store(new Child
                         {
-                            CreatedAt = DateTime.Today.AddDays(rand.Next(356)),
-                            Score = rand.Next(0, 5000),
-                            Name = RandomName(rand)
+                            Name = RandomName(rand),
+                            Birthday = DateTime.Today.AddDays(rand.Next(365)),
+                            Father = new Father
+                            {
+                                Name = RandomName(rand)
+                            },
+                            Mother = new Mother
+                            {
+                                Name = RandomName(rand)
+                            }
                         });
                     }
                 }
+
                 Console.WriteLine(sp.Elapsed);
                 Console.ReadKey();
             }
